c471ba33f7a463167ea3db191e5fdd4a9e073a9e
nick7132 <nick7132@VID7516G72>
2016-12-13 12:18:32 +0100

    Add project files.

---
+++ DiveRepo/DDMF_Light/Conn.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace DiveRepo
+{
+    public static class Conn
+    {
+        
+        public static SqlConnection GetCon()
+        {
+            SqlConnection con = new SqlConnection("server=194.255.108.50;database=dbDiveTracker;uid=DiveTracker;pwd=eGfAlC53;MultipleActiveResultSets=True");
+            return con;
+        }
+
+        public static SqlConnection CreateConnection()
+        {
+            var cn = GetCon();
+            cn.Open();
+            return cn;
+        }
+    }
+}
---
+++ DiveTracker/Models/DiveTracker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiveTracker
+{
+    public class DiveTracker
+    {
+        public int ID { get; set; }
+
+        public int IkonID { get; set; }
+
+        public string Longitude { get; set; }
+
+        public string Latitude { get; set; }
+
+        public Boolean  Sikker { get; set; }
+
+        public int  LandAfstand { get; set; }
+
+        public string Beskrivelse { get; set; }
+
+        public string Billeder { get; set; }
+
+        public string Video { get; set; }
+
+        public int Rateing { get; set; }
+
+        public string BundForhold { get; set; }
+
+        public string BundDybde { get; set; }
+
+      
+        
+        
+        
+        
+          
+         
+    }
+}
